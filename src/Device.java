9cea0eb155b9aa9bebbc97c99667af2fe19ab1c6
Dorian-666 <13Dorian-666@users.noreply.github.com>
2020-04-14 00:39:01 +0400

    Initial commit

---
+++ src/Device.java
@@ -0,0 +1,14 @@
+public class Device {
+
+    private String device;
+
+    public Device(String device){
+
+        this.device = device;
+
+    }
+
+    public String getDevice() { return device; }
+
+
+}
