e803739fcf157a36be541e78126cb86267249cc8
Dorian-666 <13Dorian-666@users.noreply.github.com>
2020-04-14 00:40:05 +0400

    Initial commit

--- src/Computer.java
+++ src/Computer.java
@@ -1,26 +1,43 @@
-public class Computer {
-    CPU cpu;
+public class Computer extends Device {
+    private CPU cpu;
 
-    Storage storage;
+    private Storage storage;
 
-    Model model;
-    public Computer(CPU givenCpu,Storage givenStroage,Model givenModel){
+    private Model model;
+    Acer acer;
 
+    public Computer(CPU givenCpu,Storage givenStroage,Model givenModel,String device) {
+        super(device);
         cpu=givenCpu;
         storage = givenStroage;
         model=givenModel;
 
     }
+
+    public CPU getCpu() {return cpu; }
+
+    public Model getModel() { return model; }
+
+    public Storage getStorage() { return storage; }
+
+
+    public Acer getAcer() { return acer; }
+
+    @Override
+    public String getDevice() { return super.getDevice(); }
+
     public void change(){
 
+
         System.out.println("Model:"+model.getComputer_model());
-        System.out.println("Corporation:"+model.getCorporation());
+
+        System.out.println("Year:"+cpu.getYearCpu());
 
         System.out.println("RAM:"+storage.getRam());
         System.out.println("ROM:"+storage.getRom());
 
         System.out.println("CPU:"+cpu.getModel());
-        System.out.println("Year:"+cpu.getYearCpu());
+
         System.out.println("Generation:"+cpu.getGeneration());
         System.out.println("Speed:"+cpu.getSpeedCpu());
     }
@@ -30,3 +47,5 @@ public class Computer {
 
 
 }
+
+
